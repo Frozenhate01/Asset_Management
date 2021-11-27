0591b6faaa255ec36906a6376f1c7aa1bb6022f0
Frozenhate01 <35885644+Frozenhate01@users.noreply.github.com>
2021-11-26 22:01:45 -0500

    UI setup
    Adding UI setup with start scene.

---
+++ Asset Management Project/Assets/Scripts/GSN_AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GSN_AudioManager : MonoBehaviour
+{
+    
+    public static GSN_AudioManager instance;
+
+    public Sound[] SFX;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        foreach (Sound s in SFX)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("main menu place holder for music");
+    }
+
+    public void Play (string sound)
+    {
+        Sound s = ArrayList.Find(sounds, item => item.name == sound);
+
+        if ( s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+    }
+}
---
+++ Asset Management Project/Assets/Scripts/GSN_MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GSN_MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Loads next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // Quits game and returns to desktop.
+        Application.Quit();
+    }
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
