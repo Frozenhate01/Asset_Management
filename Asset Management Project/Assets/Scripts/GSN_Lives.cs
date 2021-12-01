d1c2ea696f8d74e018176f62b44f01123a829495
Frozenhate01 <frozenhate01@gmail.com>
2021-11-30 23:06:28 -0500

    Adding score system and otheres
    score system, lives and health UI system with new UI to main menu / options and during game.

---
+++ Asset Management Project/Assets/Scripts/GSN_Lives.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GSN_Lives : MonoBehaviour
+{
+    public static int lives = 3;
+    public Text numberOfLives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfLives = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numberOfLives.text = "" + lives;
+    }
+
+    // Add 10 score value to score total on enemy destory
+    // GSN_Score.scoreTotal += 10;
+}
---
+++ Asset Management Project/Assets/Scripts/GSN_Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GSN_Score : MonoBehaviour
+{
+    public static int scoreTotal = 100000;
+    public Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "" + scoreTotal;
+    }
+
+    // Add 10 score value to score total on enemy destory
+    // GSN_Score.scoreTotal += 10;
+}
