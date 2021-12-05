ed166ddf1a0fabb9e5d2e9c31a426f38753e116c
glensei <gwchandler@student.fullsail.edu>
2021-12-05 15:54:22 -0500

    new character movement script
    jump works as intended

--- Asset Management Project/Assets/Scripts/Glen Scripts/BasicPlayerMovement.cs
+++ Asset Management Project/Assets/Scripts/Glen Scripts/BasicPlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BasicPlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private LayerMask layerMask;
+
     private Rigidbody2D body;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        body = transform.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -17,6 +20,6 @@ public class BasicPlayerMovement : MonoBehaviour
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
         if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+            body.velocity = new Vector2(body.velocity.x, jumpVelocity);
     }
 }
