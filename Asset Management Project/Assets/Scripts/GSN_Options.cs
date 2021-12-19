2a462ddbd8f0704aa89e39b174854fbede178d89
Frozenhate01 <frozenhate01@gmail.com>
2021-12-19 01:28:47 -0500

    Revert "Fixing error"
    This reverts commit 597fe9c9b48d3453fd58f47948e42cab5d627850.

--- Asset Management Project/Assets/Scripts/GSN_Options.cs
+++ Asset Management Project/Assets/Scripts/GSN_Options.cs
@@ -48,8 +48,6 @@ public class GSN_Options : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
-
-      
     }
 
     public void LoadMenu()
